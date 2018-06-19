890862b5f94427f2c87f1f89058f3374a2c5d628
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2018-06-19 16:02:19 +0200

    edit sequelize_commands

--- server/models/mission.js
+++ server/models/mission.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-    const Mission = sequelize.define('Mission', {
+    const Mission = sequelize.define('missions', {
         title: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -11,14 +11,22 @@ module.exports = (sequelize, DataTypes) => {
         complete: {
             type: DataTypes.BOOLEAN,
             defaultValue: false,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
         }
 
     });
 
     Mission.associate = (models) => {
         Mission.hasMany(models.MissionItem, {
-            foreignKey: 'missionId',
-            as: 'missionItems',
+            foreignKey: 'mission_id',
+            as: 'mission_items',
         });
     };
 
